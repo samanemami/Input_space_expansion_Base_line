32c18d59963abce9c611d0080828f218bd9c297d
Seyedsaman (sam) Emami <saman.emami@hotmail.com>
2022-03-06 12:33:45 +0100

    update

--- Example/Example.py
+++ Example/Example.py
@@ -1,4 +1,6 @@
 # %%
+import collections
+import itertools
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import KFold
 import pandas as pd
@@ -11,14 +13,14 @@ X, y = dts.make_regression(n_samples=500,
                            n_targets=5)
 
 
-title = 'atp7d'
+title = 'multivariate'
 cv_out = 2
 
 random_state = 123
 np.random.seed(random_state)
 
-df = pd.DataFrame(y)
-(df.corr(method='pearson', min_periods=1)).to_csv(title + "_Correlations.csv")
+# df = pd.DataFrame(y)
+# (df.corr(method='pearson', min_periods=1)).to_csv(title + "_Correlations.csv")
 
 
 def input(X, y, i):
@@ -36,38 +38,28 @@ for _, (train_index, test_index) in enumerate(kfold.split(X, y)):
     y_train, y_test = y[train_index], y[test_index]
 
     # Train m models for m outputs
-    # for i in range(0, y_train.shape[1], 1):
+    for i in range(0, y_train.shape[1], 1):
 
-    # model = RandomForestRegressor()
-    # model.fit(x_train, y_train[:, i])
-    # score = model.score(x_test, y_test[:, i])
-    # scores.iloc[_, i] = score
-    # mapping = {scores.columns[i]: 'target_'+str(i)}
-    # scores = scores.rename(columns=mapping)
+        model = RandomForestRegressor()
+        model.fit(x_train, y_train[:, i])
+        score = model.score(x_test, y_test[:, i])
+        scores.iloc[_, i] = score
+        mapping = {scores.columns[i]: 'target_'+str(i)}
+        scores = scores.rename(columns=mapping)
 
-    # 1st experiment: we can see how much
-    #   informations there are in outputs to predict other outputs
+    # 2nd experiment: Check how much informations there are
+    # in outputs to predict other outputs.
 
-    # j = 0
+    j = 0
+    while j < y_train.shape[1]:
+        if j+1 <= y_train.shape[1]:
+            X_train = np.delete(y_train, j, axis=1)
+            Y_train = y_train[:, j]
 
-    # while j < y_train.shape[1]:
-
-    #   if j+1 < y_train.shape[1]:
-    #     X_train =
-
-# %%
-
-
-# %%
-i = 0
-while i < y_train.shape[1]:
-    if i+1 < y_train.shape[1]:
-        X = np.append(y_train[:, i][:, np.newaxis],
-                      y_train[:, i+1][:, np.newaxis], axis=1)
-        i += 1
-    else:
-        break
+            model.fit(X_train, Y_train)
+            j += 1
+        else:
+            break
 
 
 # %%
-X.shape
