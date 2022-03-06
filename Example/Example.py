74990e594900a18b7187d0a9ca132fa94c004fee
Seyedsaman (sam) Emami <saman.emami@hotmail.com>
2022-03-06 12:55:17 +0100

    update the training

--- Example/Example.py
+++ Example/Example.py
@@ -1,6 +1,3 @@
-# %%
-import collections
-import itertools
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import KFold
 import pandas as pd
@@ -14,29 +11,31 @@ X, y = dts.make_regression(n_samples=500,
 
 
 title = 'multivariate'
-cv_out = 2
+cv = 2
 
 random_state = 123
 np.random.seed(random_state)
 
-# df = pd.DataFrame(y)
-# (df.corr(method='pearson', min_periods=1)).to_csv(title + "_Correlations.csv")
+df = pd.DataFrame(y)
+(df.corr(method='pearson', min_periods=1)).to_csv(title + "_Correlations.csv")
 
 
-def input(X, y, i):
-    X = np.append(X, y[:, i][:, np.newaxis], axis=1)
+def augment(X, y):
+    X = np.append(X, y, axis=1)
     return X
 
 
-kfold = KFold(n_splits=cv_out, shuffle=True, random_state=random_state)
-n_targest = y.shape[1] * 2
-scores = np.zeros((cv_out, n_targest))
+kfold = KFold(n_splits=cv, shuffle=True, random_state=random_state)
+n_targest = y.shape[1] * 3
+scores = np.zeros((cv, n_targest))
 scores = pd.DataFrame(scores, columns=pd.RangeIndex(0, scores.shape[1], 1))
+
 
 for _, (train_index, test_index) in enumerate(kfold.split(X, y)):
     x_train, x_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
 
+    # 1st Experiment:
     # Train m models for m outputs
     for i in range(0, y_train.shape[1], 1):
 
@@ -47,19 +46,57 @@ for _, (train_index, test_index) in enumerate(kfold.split(X, y)):
         mapping = {scores.columns[i]: 'target_'+str(i)}
         scores = scores.rename(columns=mapping)
 
-    # 2nd experiment: Check how much informations there are
-    # in outputs to predict other outputs.
+    # 2nd Experiment:
+    # Check the information in outputs to predict another outputs.
 
+    i += 1
     j = 0
     while j < y_train.shape[1]:
         if j+1 <= y_train.shape[1]:
             X_train = np.delete(y_train, j, axis=1)
             Y_train = y_train[:, j]
 
+            X_test = np.delete(y_test, j, axis=1)
+            Y_test = y_test[:, j]
+
             model.fit(X_train, Y_train)
+            score = model.score(X_test, Y_test)
+
+            scores.iloc[_, i] = score
+
+            mapping = {scores.columns[i]: 'Y|target'+str(j)}
+            scores = scores.rename(columns=mapping)
+
+            i += 1
             j += 1
         else:
             break
 
+    # 3rd Experiment:
+    # Inspect how much informations there are in outputs
+    # to predict other outputs over the baseline.
 
-# %%
+    j = 0
+    while j < y_train.shape[1]:
+        if j+1 <= y_train.shape[1]:
+            X_train = np.delete(y_train, j, axis=1)
+            X_train = augment(x_train, X_train)
+            Y_train = y_train[:, j]
+
+            X_test = np.delete(y_test, j, axis=1)
+            X_test = augment(x_test, X_test)
+            Y_test = y_test[:, j]
+
+            model.fit(X_train, Y_train)
+            score = model.score(X_test, Y_test)
+
+            scores.iloc[_, i] = score
+
+            mapping = {scores.columns[i]: 'D\'_target'+str(j)}
+            scores = scores.rename(columns=mapping)
+
+            i += 1
+            j += 1
+
+        else:
+            break
